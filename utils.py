cf74e68fa6516d3b012eef3a4a77850630bb7008
David <92113063+gomesdevs@users.noreply.github.com>
2025-04-12 19:28:56 -0300

    Fazendo o Deploy utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
+# utils.py
+from datetime import datetime, timedelta
 
+def get_default_dates() -> tuple:
+   
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
+    return start_date, end_date
